6d4f24a0ca0d61fc7a0ec05e60658cd4a9e7313c
Maxi Scalzotto <maximiliano.scalzotto@almundo.com>
2017-09-20 18:05:52 -0300

    Commit inicial

---
+++ src/components/Cosas.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="cosas">
+    <label for ="input-cosas">Fibonacci</label>
+    <input v-model="fibNum" type="text" id="input-cosas" />
+    {{ fibonacci(fibNum) }}
+  </div>  
+</template>
+
+<script>
+  export default {
+    name: 'cosas',
+    data () {
+      return {
+        fibNum: ''
+      }
+    },
+    methods: {
+      fibonacci: function (n) {
+        if (n <= 1) return 1
+        return this.fibonacci(n - 1) + this.fibonacci(n - 2)
+      }
+    }
+  }
+</script>
+
+<styles>
+
+</styles>
